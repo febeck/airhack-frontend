7941d05e8ba6703fdd47ff8ed58e9eac640fabd9
Emilien Chauvet <emilien.chauvet.enpc@gmail.com>
2019-07-13 09:24:07 +0200

    Add areas

--- src/tasks.js
+++ src/tasks.js
@@ -4,13 +4,15 @@ export const tasks = [
         "lat": 48.87450294445255,
         "lng": 2.3211854209178524,
         "id": "67911832523706",
-        "assignee_id": 1
+        "assignee_id": 1,
+        area: "Paris 16",
     },
     {
         "dueTime": "11:15",
         "lat": 48.87586307653257,
         "lng": 2.3706216789918724,
         "id": "67555738843276",
+        area: "Paris 12",
         "assignee_id": 1
     },
     {
@@ -18,6 +20,7 @@ export const tasks = [
         "lat": 48.87767222056119,
         "lng": 2.360865678383674,
         "id": "65100773683458",
+        area: "Paris 18",
         "assignee_id": 1
     },
     {
@@ -25,6 +28,7 @@ export const tasks = [
         "lat": 48.87366083646156,
         "lng": 2.337528916419766,
         "id": "82014542640687",
+        area: "Clichy",
         "assignee_id": 1
     },
     {
@@ -32,6 +36,7 @@ export const tasks = [
         "lat": 48.87678392270649,
         "lng": 2.340454708930852,
         "id": "16076616042927",
+        area: "Paris 16",
         "assignee_id": 1
     },
     {
@@ -39,6 +44,7 @@ export const tasks = [
         "lat": 48.8764587513743,
         "lng": 2.338487307137354,
         "id": "2032377408003",
+        area: "Paris 8",
         "assignee_id": 1
     },
     {
@@ -46,6 +52,7 @@ export const tasks = [
         "lat": 48.86573137787089,
         "lng": 2.3514118747843598,
         "id": "55757344594506",
+        area: "Levallois",
         "assignee_id": 1
     },
     {
@@ -53,6 +60,7 @@ export const tasks = [
         "lat": 48.86964073094136,
         "lng": 2.3473605748438047,
         "id": "83689612947166",
+        area: "Courbevoie",
         "assignee_id": 1
     },
     {
@@ -60,6 +68,7 @@ export const tasks = [
         "lat": 48.87487265727264,
         "lng": 2.361042437108914,
         "id": "44972131355378",
+        area: "Paris 3",
         "assignee_id": 1
     }
 ]
