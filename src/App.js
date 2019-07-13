cb51b93a7c3fbaae17a6b07bcb583cef43414393
Fernando Beck <fe.beck25@gmail.com>
2019-07-13 06:17:26 +0200

    feat: retrieve data from API

--- src/App.js
+++ src/App.js
@@ -1,92 +1,93 @@
-import React from 'react';
-import {Tabs, Tab, Button, Table} from 'react-bootstrap';
+import React from 'react'
+import { Tabs, Tab, Button, Table } from 'react-bootstrap'
 import Map from 'pigeon-maps'
 import Marker from 'pigeon-marker/react'
-import {tasks} from './tasks'
-import axios from 'axios';
+import axios from 'axios'
 
-import './App.css';
+import './App.css'
 
 class App extends React.Component {
   constructor(props, context) {
-    super(props, context);
+    super(props, context)
     this.state = {
       key: 'map',
       width: 0,
       height: 0,
-      selectedTask:null,
+      selectedTask: null,
       zoom: 12,
-      center: [48.856614, 2.3522219]
-    };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+      center: [48.856614, 2.3522219],
+      tasks: [],
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
   }
 
   componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-    this.setState({tasks})
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
     const options = {
       method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-      },
+      headers: { 'content-type': 'application/json', 'access-control-allow-origin': '*' },
       url: 'https://guarded-garden-24957.herokuapp.com/myTasks?userId=1',
     }
-    axios(options)
-      .then(tasks => this.setState({tasks}))
+    axios(options).then(({ data }) => this.setState({ tasks: data }))
   }
 
   updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    this.setState({ width: window.innerWidth, height: window.innerHeight })
   }
 
   handleMarkerClick(taskId) {
-    this.setState({selectedTask: taskId})
+    this.setState({ selectedTask: taskId })
   }
 
   render() {
-    console.log(this.state)
+    const { tasks } = this.state
     return (
-        <div>
-          <Map center={this.state.center} zoom={this.state.zoom} width={this.state.width} height={this.state.height * 0.5}>
-            {
-              tasks.map(task => (
-                <Marker anchor={[task.lat, task.lng]} hover={task.id === this.state.selectedTask} onClick={() => this.handleMarkerClick(task.id)} />
-              ))
-            }
-          </Map>
-          <div style={{width:this.state.width, height: this.state.height * 0.5, overflow: "scroll"}}>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Time</th>
-                  <th>Phone</th>
-                  <th>Check</th>
+      <div>
+        <Map
+          center={this.state.center}
+          zoom={this.state.zoom}
+          width={this.state.width}
+          height={this.state.height * 0.5}
+        >
+          {tasks.map(task => (
+            <Marker
+              anchor={[task.lat, task.lng]}
+              hover={task.id === this.state.selectedTask}
+              onClick={() => this.handleMarkerClick(task.id)}
+            />
+          ))}
+        </Map>
+        <div style={{ width: this.state.width, height: this.state.height * 0.5, overflow: 'scroll' }}>
+          <Table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Phone</th>
+                <th>Check</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map(task => (
+                <tr
+                  style={task.id === this.state.selectedTask ? { backgroundColor: 'lightblue' } : {}}
+                  onClick={() => {
+                    this.setState({ selectedTask: task.id, zoom: 14, center: [task.lat, task.lng] })
+                  }}
+                >
+                  <th>{task.dueTime}</th>
+                  <td>0628764802</td>
+                  <td>
+                    <Button variant="primary">Done</Button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {tasks.map(task => (
-                  <tr
-                    style={
-                      task.id === this.state.selectedTask
-                      ? {backgroundColor: "lightblue"}
-                      : {}
-                    }
-                    onClick={() => {
-                      this.setState({selectedTask: task.id, zoom: 14, center:[task.lat, task.lng]})
-                    }}
-                  >
-                    <th>{task.dueTime}</th>
-                    <td>0628764802</td>
-                    <td><Button variant="primary">Done</Button></td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
+              ))}
+            </tbody>
+          </Table>
         </div>
-    );
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
