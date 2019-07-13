145280e2e06939168a7faf691b5fbb16ab9db086
Emilien Chauvet <emilien.chauvet.enpc@gmail.com>
2019-07-13 05:32:55 +0200

    Enhance design

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends React.Component {
     this.state = {
       key: 'map',
       width: 0,
-      height: 0
+      height: 0,
+      selectedTask:null,
+      zoom: 12,
+      center: [48.856614, 2.3522219]
     };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
@@ -27,47 +30,51 @@ class App extends React.Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
+  handleMarkerClick(taskId) {
+    this.setState({selectedTask: taskId})
+  }
+
   render() {
     console.log(this.state)
     return (
-        <Tabs
-          id="controlled-tab-example"
-          activeKey={this.state.key}
-          onSelect={key => this.setState({ key })}
-        >
-          <Tab eventKey="map" title="Map">
-            <div className="App">
-              <Map center={[48.856614, 2.3522219]} zoom={11} width={this.state.width} height={this.state.height * 0.75}>
-                {
-                  tasks.map(task => (
-                    <Marker anchor={[task.lat, task.lng]} payload={1} onClick={this.handleMarkerClick} />
-                  ))
-                }
-              </Map>
-              <Button color="info">Call next guest</Button>{' '}
-            </div>
-          </Tab>
-          <Tab eventKey="schedule" title="Schedule">
+        <div>
+          <Map center={this.state.center} zoom={this.state.zoom} width={this.state.width} height={this.state.height * 0.5}>
+            {
+              tasks.map(task => (
+                <Marker anchor={[task.lat, task.lng]} hover={task.id === this.state.selectedTask} onClick={() => this.handleMarkerClick(task.id)} />
+              ))
+            }
+          </Map>
+          <div style={{width:this.state.width, height: this.state.height * 0.5, overflow: "scroll"}}>
             <Table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Address</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map(task => (
-            <tr>
-              <th>{task.dueTime}</th>
-              <td>Paris</td>
-              <td>0628764802</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-          </Tab>
-        </Tabs>
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>Phone</th>
+                  <th>Check</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map(task => (
+                  <tr
+                    style={
+                      task.id === this.state.selectedTask
+                      ? {backgroundColor: "lightblue"}
+                      : {}
+                    }
+                    onClick={() => {
+                      this.setState({selectedTask: task.id, zoom: 14, center:[task.lat, task.lng]})
+                    }}
+                  >
+                    <th>{task.dueTime}</th>
+                    <td>0628764802</td>
+                    <td><Button variant="primary">Done</Button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
     );
   }
 }
